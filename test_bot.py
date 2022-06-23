33d6e69ec28c2d7e68db88533d03a3dcf656a8b8
Ravshan <battaloff@mail.ru>
2022-06-23 22:22:25 +0500

    Туплю, устал

--- test_bot.py
+++ test_bot.py
@@ -23,48 +23,55 @@ from tools import DBTools
 bot = Bot("5313791416:AAGynLjrG2Jo5C0BhKiEgRPAlsjTgku-_wQ")
 dp = Dispatcher(bot)
 
+# Здесь хранятся пользовательские данные.
+# Т.к. это словарь в памяти, то при перезапуске он очистится
 user_data = {}
 
 
-def get_keyboard():
+# fabnum - префикс, action - название аргумента, которым будем передавать значение
+callback_numbers = CallbackData("fabnum", "action")
+
+
+def get_keyboard_fab():
     buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
-        types.InlineKeyboardButton(text="+1", callback_data="num_incr"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")
+        types.InlineKeyboardButton(text="-1", callback_data=callback_numbers.new(action="decr")),
+        types.InlineKeyboardButton(text="+1", callback_data=callback_numbers.new(action="incr")),
+        types.InlineKeyboardButton(text="Подтвердить", callback_data=callback_numbers.new(action="finish"))
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
     return keyboard
 
 
-async def update_num_text(message: types.Message, new_value: int):
+async def update_num_text_fab(message: types.Message, new_value: int):
     with suppress(MessageNotModified):
-        await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard())
+        await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard_fab())
 
 
-@dp.message_handler(commands="numbers")
+@dp.message_handler(commands="numbers_fab")
 async def cmd_numbers(message: types.Message):
     user_data[message.from_user.id] = 0
-    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard_fab())
 
 
-@dp.callback_query_handler(Text(startswith="num_"))
-async def callbacks_num(call: types.CallbackQuery):
-    print(call.data)
+@dp.callback_query_handler(callback_numbers.filter(action=["incr", "decr"]))
+async def callbacks_num_change_fab(call: types.CallbackQuery, callback_data: dict):
     user_value = user_data.get(call.from_user.id, 0)
-    action = call.data.split("_")[1]
-
+    action = callback_data["action"]
     if action == "incr":
-        user_data[call.from_user.id] = user_value+1
-        await update_num_text(call.message, user_value+1)
+        user_data[call.from_user.id] = user_value + 1
+        await update_num_text_fab(call.message, user_value + 1)
     elif action == "decr":
-        user_data[call.from_user.id] = user_value-1
-        await update_num_text(call.message, user_value-1)
-    elif action == "finish":
-        await call.message.edit_text(f"Итого: {user_value}")
-
+        user_data[call.from_user.id] = user_value - 1
+        await update_num_text_fab(call.message, user_value - 1)
     await call.answer()
 
+
+@dp.callback_query_handler(callback_numbers.filter(action=["finish"]))
+async def callbacks_num_finish_fab(call: types.CallbackQuery):
+    user_value = user_data.get(call.from_user.id, 0)
+    await call.message.edit_text(f"Итого: {user_value}")
+    await call.answer()
 
 
 executor.start_polling(dp, skip_updates=True)
